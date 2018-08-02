9dcbbd5a77d5b106a3b65c99474ac5f33f57fdc7
danilo <danilo.miranda.munoz@gmail.com>
2018-08-02 17:05:57 -0400

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -9,7 +9,7 @@ let urlDB;
 if (process.env.NODE_ENV == 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb://cafeuser:a123456@ds123084.mlab.com:23084/dmirandam-node-cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.urlDB = urlDB;
